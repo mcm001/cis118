0f97b4079bdf1baf07c799988a649f4e2aded255
mcm001 <matthew.morley.ca@gmail.com>
2018-09-29 21:39:47 -0700

    week 7 prob 1 solution

--- week7/week7MatthewMorleyLanguageProg1Fix.cpp
+++ week7/week7MatthewMorleyLanguageProg1Fix.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int main ( ) { // start program //Fixed spelling of int and loop
     // Declare and Initialize Variables //Fixed comment slashes
     int height = 0;
-    int feet = 0
-    Int inches = 0;
+    int feet = 0; //Fixed semi colin
+    int inches = 0;//fixed spelling
 
     // Prompt for height
-    cout >> “Enter your height in inches: ‘;
-    cin >> Height;
+    cout << "Enter your height in inches: ";//fixed ' charicter
+    cin >> height;//Fixed height spelling
 
     // Calculate Height in feet and inches
-    height / 12 = feet;
+    feet = height / 12 ;//reordered expression
     inches = height % 12;
 
     //Print out height in feet and inches //Fixed comments
-    cout << “You are “ < FEET << “ feet and “ <> inches >> “inches” ; //Fixed the spelling of cout
+    cout << "You are " << feet << " feet and " << inches << " inches"; //Fixed '' and "", Fixed <>, Fixed the spelling of cout, fixed << operator
 
-    system(“pause”); // Mac user comment out this line by placing // in front of it.
+    //system(“pause”); // Mac user comment out this line by placing // in front of it.
     // PC users -Hint replace the “ with the ones on your computer–coping code cause error, so just type it.
     return 0;
 } // end of program
