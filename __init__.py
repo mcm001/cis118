3f3ff8afb2d424cf3b3e69c8c2ceb47ab9894a29
mcm001 <matthew.morley.ca@gmail.com>
2018-09-29 21:28:43 -0700

    moved files around

--- __init__.py
+++
@@ -1,6 +0,0 @@
-import os
-import matplotlib as mpl
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using non-interactive Agg backend')
-    mpl.use('Agg')
-import matplotlib.pyplot as plt
