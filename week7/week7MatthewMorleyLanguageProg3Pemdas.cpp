624f8cbad12e15b1a3d9c9ac8859047450b734bf
mcm001 <matthew.morley.ca@gmail.com>
2018-09-30 22:15:37 -0700

    did week 7

--- week7/week7MatthewMorleyLanguageProg3Pemdas.cpp
+++ week7/week7MatthewMorleyLanguageProg3Pemdas.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main ( ) {
+    //Define variables
+    double a = 18;
+    double b = 6;
+    double c = 3;
 
-
-
-}
+    cout << "--------------" << endl;
+    cout << "This one outputs a + (b/c)" << endl;
+    cout << a + b / c  << endl;
+    cout << "This outputs (a+b) divided by c" << endl;
+    cout << ( a + b ) / c  << endl;
+    cout << "this one outputs a plus (b/c)" << endl;
+    cout << a + ( b / c ) << endl;
+    cout << "Order of operations!" << endl;
+    
+}   
