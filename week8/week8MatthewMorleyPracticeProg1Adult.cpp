cd1854b02b3e0c75c579a1d55d59bc3550720869
mcm001 <matthew.morley.ca@gmail.com>
2018-10-02 11:55:28 -0700

    problem 1 code added

---
+++ week8/week8MatthewMorleyPracticeProg1Adult.cpp
@@ -0,0 +1,18 @@
+#include <iostream> //Fixed include statement
+using namespace std;
+
+int main ( ) {
+    string firstName;
+    int age;
+
+    //Get the users name
+    cout << "Enter your first name, please";
+    cin >> firstName;
+
+    //Get the users age
+    cout << "Enter your age in years, please";
+    cin >> age;
+
+    if age >= 15 { cout << firstName << ", you are an adult."; }
+    else { cout << firstName << ", you are not an adult."; }
+}
