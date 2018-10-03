27ed0281c0fedf41c84511b017297afe9911f2d9
mcm001 <matthew.morley.ca@gmail.com>
2018-10-03 08:44:27 -0700

    Finished week8 prog1

--- week8/week8MatthewMorleyPracticeProg1Adult.cpp
+++ week8/week8MatthewMorleyPracticeProg1Adult.cpp
@@ -6,13 +6,17 @@ int main ( ) {
     int age;
 
     //Get the users name
-    cout << "Enter your first name, please";
+    cout << "Enter your first name, please: ";
     cin >> firstName;
 
     //Get the users age
     cout << "Enter your age in years, please";
     cin >> age;
 
-    if age >= 15 { cout << firstName << ", you are an adult."; }
-    else { cout << firstName << ", you are not an adult."; }
+    if (age >= 15) { 
+        cout << firstName << ", you are an adult.";
+    }
+    else { 
+        cout << firstName << ", you are not an adult."; 
+    }
 }
