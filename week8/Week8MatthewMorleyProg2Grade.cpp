e8928b4d3a2296748ef72d9739b20a3a5051821a
mcm001 <matthew.morley.ca@gmail.com>
2018-10-04 10:41:05 -0700

    added up to problem 4

---
+++ week8/Week8MatthewMorleyProg2Grade.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //Fixed include statement
+using namespace std;
+
+int main ( ) {
+ 
+    int grade;
+    cout << "What is your grade? \n";
+    cin >> grade;
+
+    if (grade < 60) {}
+
+
+}
