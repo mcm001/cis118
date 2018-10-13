659254bb924fc3ff110a62ee36b80c62f55c891b
Matthew Morley <matthew.morley.ca@gmail.com>
2018-10-12 21:42:31 -0700

    week9prob1

---
+++ week9/Week9MatthewMorleySeq3EvalExp10.cpp
@@ -0,0 +1,27 @@
+#include <iostream> //Fixed include statement
+using namespace std;
+
+int main ( ) {
+    
+
+    cout << (13 > 42)|| (4 < 9) << "\n"
+    
+    cout << 2.(11 > 3) && (7 <= 7) << "\n"
+    
+    cout << 3.! false  << "\n"
+    
+    cout << 4.! (31 != 4)  << "\n"
+    
+    cout << 5.(9 != 7)&& ! (0) << "\n"
+    
+    cout << 6.(2) << "\n"
+    
+    cout << 7.(0) << "\n"
+    
+    cout << 8.(0) || (1) || (42) << "\n"
+    
+    cout << 9.!(!(0) << "\n"
+    
+    cout << 10.(1)&&(1) &&(1)&&(1)&&(1)&&(0) << "\n"
+
+}
---
+++ week9/cppTemplate.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //Fixed include statement
+using namespace std;
+
+int main ( ) {
+    
+
+}
