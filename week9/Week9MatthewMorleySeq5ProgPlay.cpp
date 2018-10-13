6a0f4792725c725b5e3521bb0748ac9d6c13e5f9
Matthew Morley <matthew.morley.ca@gmail.com>
2018-10-12 22:24:04 -0700

    heh oops

--- week9/Week9MatthewMorleySeq5ProgPlay.cpp
+++ week9/Week9MatthewMorleySeq5ProgPlay.cpp
@@ -2,33 +2,30 @@
 using namespace std;
 
 int main () {
-  while (1){
-    float age;
-    float height;
-    cout << "\n What is your age? ";
-    cin >> age;
-    cout << "What is your height in ft? ";
-    cin >> height;
+  while (1) {
+    string roomclean;
+    string homework;
+    string day;
 
-    if (age > 18 && age == 18 && height > 5) {
-      cout << "A person’s age is greater then and equal to 18 and the person is taller than 5 feet.";
-    }
-    if (age < 18 || height < 6) {
-      cout << "A person is younger than 18 or the person is shorter than 6 feet.";
+    cout << "\nWhat day is it? ";
+    cin >> day;
 
-    }
-    if (height > 6 && height < 7 && age > 18 && age < 65) {
-      cout << "A person is between 6 and 7 feet, and between 18 and 65 years old";
-    }
-    if (age != 0) {
-      cout << "A person’s age is not 0. ";
-    }
-    if (age == 18 || age == 29 || age == 42) {
-      cout << "A person age is 18 or 29 or 42.";
-    }
+    cout << "Is your room clean? ";
+    cin >> roomclean;
 
+    cout << "Do your homework? ";
+    cin >> homework;
 
+    if ((day == "monday" || day == "tuesday" || day == "wednesday" ) && (roomclean == "yes" && homework == "yes")) {
+      cout << "You can go and play.";
+    }
+    else if ((day == "thursday" || day == "friday" || day == "saturday" || day == "sunday") && (roomclean == "yes" || homework == "yes")) {
+      cout << "You can go and play.";
+    }
+    else {
+      cout << "NO PLAY!";
+    }
+    
+    
   }
-
-
 }
