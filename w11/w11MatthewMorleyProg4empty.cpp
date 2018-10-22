66787d6e3314e847f87091665b31515fdd0ccfca
mcm001 <matthew.morley.ca@gmail.com>
2018-10-22 10:01:03 -0700

    problem 5

---
+++ w11/w11MatthewMorleyProg4empty.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i = 1;
+    for ( ; ; ) { 
+        cout << "i = " << i << endl;
+        i=i+1;
+        if (i >  10) { break; }
+    }
+    return 0;
+}
---
+++ w11/w11MatthewMorleyProg5NiceMultTable.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 10; i++) {  // Switched operators to <= 10 rather than > 0
+        cout << "i = " << i << endl;
+    }
+    return 0;
+}
