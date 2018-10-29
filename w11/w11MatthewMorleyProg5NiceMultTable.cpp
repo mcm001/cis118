3ed584b8697136337a4f7053971e4b2bc90c3570
Matthew Morley <matthew.morley.ca@gmail.com>
2018-10-28 21:20:48 -0700

    fixed problem 5

--- w11/w11MatthewMorleyProg5NiceMultTable.cpp
+++ w11/w11MatthewMorleyProg5NiceMultTable.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main() {
-    for (int i = 1; i <= 10; i++) {  // Switched operators to <= 10 rather than > 0
-        cout << "i = " << i << endl;
+    for (int x = 1; x <= 12; x++) {  // Switched operators to <= 10 rather than > 0
+		for (int y = 1; y <=12; y++) {
+			cout << setw(4) << y*x  ;
+		}
+		cout << endl;
     }
     return 0;
 }
