1d85e92e1a572ac3bfac43c716a38fce37e31223
mcm001 <matthew.morley.ca@gmail.com>
2018-11-18 20:57:31 -0800

    prog 1

--- w14/w14MatthewMorleyProg1SquareFn.cpp
+++ w14/w14MatthewMorleyProg1SquareFn.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+float calcArea ( int radius ) {
+    return radius * radius;
+}
+
+float calcDiameter ( int radius ) {
+    return radius * sqrt(2);
+}
+
+float calcCircum ( int radius ) {
+    return radius * 4;
+}
+
 int main() {
+    cout << "what is the length of one side of the square?" << endl;
+    float side;
+    cin >> side;
+
+    cout << "the area is " << calcArea(side):
+    cout << "the diameter is " << calcDiameter(side);
+    cout << "the circumference is " << calcCircum(side);
+
+    return 0;
+}
