db4314d061ef8133df2ecdad175eb8e53b064b18
mcm001 <matthew.morley.ca@gmail.com>
2018-11-18 21:14:35 -0800

    prog 4

--- w14/w14MatthewMorleyProg4Roman.cpp
+++ w14/w14MatthewMorleyProg4Roman.cpp
@@ -1,3 +1,42 @@
 #include <iostream>
+
 using namespace std;
+
+string intToNumeral ( int year ) {
+    string roman;
+    int workingNum = year;
+    while ( workingNum >= 1000 ) {
+        workingNum = workingNum - 1000;
+        roman.append("M");
+    }
+    while ( workingNum >= 100 ) {
+        workingNum = workingNum - 100;
+        roman.append("C");
+    }
+    while ( workingNum >= 50 ) {
+        workingNum = workingNum - 50;
+        roman.append("L");
+    }
+    while ( workingNum >= 10 ) {
+        workingNum = workingNum - 10;
+        roman.append("X");
+    }
+    while ( workingNum >= 5 ) {
+        workingNum = workingNum - 5;
+        roman.append("V");
+    }
+    while ( workingNum >= 0 ) {
+        workingNum = workingNum - 1;
+        roman.append("I");
+    }
+
+}
 int main() {
+    cout << "what is the year? \n";
+    string year;
+    cin >> year;
+
+    cout << "The year is actually " << intToNumeral(year) << endl;
+
+    return 0;
+}
