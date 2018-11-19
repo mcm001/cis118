d80168dc35b8deb84a7ac860f7af3859a98959fc
mcm001 <matthew.morley.ca@gmail.com>
2018-11-18 21:02:40 -0800

    prog 2

--- w14/w14MatthewMorleyProg2CircFn.cpp
+++ w14/w14MatthewMorleyProg2CircFn.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
+float clacArea ( int radius ) {
+    return M_PI * pow(radius,2);
+}
+
+float calcDiam ( int radius ) {
+    return radius * 2;
+}
+
+float calcCircum ( int radius ) {
+    return M_PI * calcDiam(radius);
+}
+
 int main() {
+    cout << "Pi is " << M_PI << endl;
+    cout << "what is the radius?\n";
+    int radius;
+    cin >> radius;
+    cout << "area: " << calcArea(radius) << endl;
+    cout << "diameter: " << calcDiam(radius) << endl;
+    cout << "circumference: " << calcCircum(radius) << endl;
+}
